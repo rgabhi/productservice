efcf42f633fd3c6a24c115333212c83e95bcd3cc
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-06 23:50:39 +0530

    add main application

---
+++ src/main/java/learning/productservice/ProductserviceApplication.java
@@ -0,0 +1,14 @@
+package learning.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductserviceApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProductserviceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/learning/productservice/models/Category.java
@@ -0,0 +1,11 @@
+package learning.productservice.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Category {
+    private long id;
+    private String name;
+}
