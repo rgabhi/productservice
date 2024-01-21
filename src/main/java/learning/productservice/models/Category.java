ff18a1e1b0f3567660471637f580d81c09899058
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-21 19:02:35 +0530

    annotate Entity for db mapping

--- src/main/java/learning/productservice/models/Category.java
+++ src/main/java/learning/productservice/models/Category.java
@@ -1,11 +1,15 @@
 package learning.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category {
+    @Id
     private long id;
     private String name;
 }
--- src/main/java/learning/productservice/models/Product.java
+++ src/main/java/learning/productservice/models/Product.java
@@ -1,14 +1,22 @@
 package learning.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product {
+    @Id
     private long id;
     private String title;
     private double price;
+    //here category is a non-primitive typo,
+    // so handle it with the concept of cardinality,
+    @ManyToOne
     private Category category;
     private String description;
     private String imageUrl;
