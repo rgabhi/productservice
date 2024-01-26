7d6a07368b7970218614b15edd67cf11b068647a
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-26 11:45:15 +0530

    update basemodel class

--- src/main/java/learning/productservice/models/BaseModel.java
+++ src/main/java/learning/productservice/models/BaseModel.java
@@ -1,5 +1,7 @@
 package learning.productservice.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -13,6 +15,7 @@ import java.util.Date;
 @MappedSuperclass
 public class BaseModel {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private Date createdAt;
     private Date lastUpdatedAt;
