9c75207e91c22b9214d2ee0de6be7c84a2cd2676
Abhinav Gupta <abhinavrgupta98@gmail.com>
2024-01-26 11:31:59 +0530

    update Product controller

--- src/main/java/learning/productservice/controllers/ProductController.java
+++ src/main/java/learning/productservice/controllers/ProductController.java
@@ -4,9 +4,11 @@ import learning.productservice.dtos.ExceptionDto;
 import learning.productservice.dtos.FakeStoreProductDto;
 import learning.productservice.exceptions.ProductNotCreatedException;
 import learning.productservice.exceptions.ProductNotFoundException;
+import learning.productservice.models.Category;
 import learning.productservice.models.Product;
 import learning.productservice.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,36 +21,63 @@ import java.util.List;
 public class ProductController {
     ProductService productService;
     @Autowired
-    ProductController(ProductService productService){
+    ProductController(@Qualifier("selfProductService") ProductService productService){
         this.productService = productService;
     }
    @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() throws ProductNotFoundException{
-        return productService.getAllProducts();
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
     }
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable("id") long id) throws ProductNotFoundException {
-        return productService.getSingleProduct(id);
+        return new ResponseEntity<>(productService.getSingleProduct(id), HttpStatus.OK);
     }
 
     @PostMapping()
     public ResponseEntity<Product> addNewProduct(@RequestBody FakeStoreProductDto fakeStoreProductDto) throws ProductNotCreatedException {
-        return productService.addProduct(fakeStoreProductDto);
+        Product product= new Product();
+        Category category = new Category();
+        category.setName(fakeStoreProductDto.getCategory());
+        product.setTitle(fakeStoreProductDto.getTitle());
+        product.setDescription(fakeStoreProductDto.getDescription());
+        product.setImageUrl(fakeStoreProductDto.getImage());
+        product.setCategory(category);
+        product.setPrice(fakeStoreProductDto.getPrice());
+        return new ResponseEntity<>(productService.addProduct(product), HttpStatus.OK);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id,@RequestBody FakeStoreProductDto fakeStoreProductDto){
-       return productService.updateProduct(id,fakeStoreProductDto);
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id,@RequestBody FakeStoreProductDto fakeStoreProductDto) throws ProductNotFoundException {
+        Product product = new Product();
+        product.setTitle(fakeStoreProductDto.getTitle());
+        product.setDescription(fakeStoreProductDto.getDescription());
+        product.setPrice(fakeStoreProductDto.getPrice());
+        product.setImageUrl(fakeStoreProductDto.getImage());
+        if(fakeStoreProductDto.getCategory() != null){
+            Category category= new Category();
+            category.setName(fakeStoreProductDto.getCategory());
+            product.setCategory(category);
+        }
+       return new ResponseEntity<>(productService.updateProduct(id,product), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Product> replaceProduct(@PathVariable("id") Long id ,@RequestBody FakeStoreProductDto fakeStoreProductDto){
-       return productService.replaceProduct(id, fakeStoreProductDto);
+    public ResponseEntity<Product> replaceProduct(@PathVariable("id") Long id ,@RequestBody FakeStoreProductDto fakeStoreProductDto) throws ProductNotFoundException {
+        Product product= new Product();
+        Category category = new Category();
+        category.setName(fakeStoreProductDto.getCategory());
+        product.setTitle(fakeStoreProductDto.getTitle());
+        product.setDescription(fakeStoreProductDto.getDescription());
+        product.setImageUrl(fakeStoreProductDto.getImage());
+        product.setCategory(category);
+        product.setPrice(fakeStoreProductDto.getPrice());
+        return new ResponseEntity<>(productService.replaceProduct(id, product), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Product> deleteProduct(@PathVariable("id") Long id){
-        return productService.deleteProduct(id);
+    public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long id){
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @ExceptionHandler(ProductNotCreatedException.class)
